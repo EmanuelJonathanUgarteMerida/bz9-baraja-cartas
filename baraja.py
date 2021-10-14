a616595c0ec74b063a4eb667f1347c14fad4f0b6
Emanuel Jonathan Ugarte Mérida <emanuelugartemerida@gmail.com>
2021-10-14 20:12:57 +0200

    Se añaden métodos de comparación

---
+++ baraja.py
@@ -0,0 +1,30 @@
+class Carta:
+    def __init__(self, valor, simbolo, palo):
+        self.valor = valor
+        self.simbolo = simbolo
+        self.palo = palo
+
+    def __str__(self) -> str:
+        return f"{self.valor} {self.simbolo}{self.palo}"
+
+    def __repr__(self) -> str:
+        return self.__str__
+
+    def __eq__(self, otro: object) -> bool:
+        return self.valor == otro.valor
+
+    def __gt__(self, otro):
+        return self.valor > otro.valor
+
+    def __ge__(self, otro):
+        return self.valor >= otro.valor
+
+    def __lt__(self, otro):
+        return self.valor < otro.valor
+
+    def __le__(self, otro):
+        return self.valor <= otro.valor
+
+
+class Baraja:
+    pass
